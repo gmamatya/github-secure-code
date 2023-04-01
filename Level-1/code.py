31e12e1ad7d242df2654bda808c3c117c23792af
gmamatya <32754791+gmamatya@users.noreply.github.com>
2023-04-01 20:39:53 +0000

    Level 1 Solution

--- Level-1/code.py
+++ Level-1/code.py
@@ -16,18 +16,27 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+def verifyorderquantities(amount: float, quantity: int):
+    max_amount = 100000
+    max_quantity = 100
+    if (amount < -max_amount) or amount > max_amount or quantity < 0 or quantity > max_quantity:
+        return False
+    return True
+
 def validorder(order: Order):
     net = 0
     
     for item in order.items:
-        if item.type == 'payment':
-            net += item.amount
-        elif item.type == 'product':
-            net -= item.amount * item.quantity
-        else:
-            return("Invalid item type: %s" % item.type)
+        if verifyorderquantities(item.amount, item.quantity):
+            if item.type == 'payment':
+                net += item.amount
+            elif item.type == 'product':
+                net -= item.amount * item.quantity
+            else:
+                return("Invalid item type: %s" % item.type)
     
     if net != 0:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
         return("Order ID: %s - Full payment received!" % order.id)
+
